734f60391cdf34df398013317a8a21c5b24d95cb
sungbo <ishimsungbo@gmail.com>
2022-11-03 13:53:22 +0900

    #스프링 부트 2.6이상부터는 3.5.5 즉 몽고디비 버전을 잘 적어야 한다.

--- src/main/java/com/spr/reactivexo/CartItem.java
+++ src/main/java/com/spr/reactivexo/CartItem.java
@@ -1,16 +1,22 @@
 package com.spr.reactivexo;
 
+import java.util.Objects;
+
 public class CartItem {
 
     private Item item;
     private int quantity;
 
-    public CartItem() {
-    }
+    private CartItem() {}
 
-    public CartItem(Item item, int quantity) {
+    CartItem(Item item) {
         this.item = item;
-        this.quantity = quantity;
+        this.quantity = 1;
+    }
+    // end::code[]
+
+    public void increment() {
+        this.quantity++;
     }
 
     public Item getItem() {
@@ -30,10 +36,22 @@ public class CartItem {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        CartItem cartItem = (CartItem) o;
+        return quantity == cartItem.quantity && Objects.equals(item, cartItem.item);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item, quantity);
+    }
+
+    @Override
     public String toString() {
-        return "CartItem{" +
-                "item=" + item +
-                ", quantity=" + quantity +
-                '}';
+        return "CartItem{" + "item=" + item + ", quantity=" + quantity + '}';
     }
 }
--- src/main/java/com/spr/reactivexo/HomeController.java
+++ src/main/java/com/spr/reactivexo/HomeController.java
@@ -36,7 +36,44 @@ public class HomeController {
     }
     // end::2[]
 
-
-
+    /**
+     * 화면에서 Add to Cart 버튼클릭 시
+     * 1.@PostMapping add/{id}로 들어오는 POST 요청을 이 메소드에 매핑해준다.
+     * 2.@PathVariable 은 {id}자리로 들어오는 값을 추출해서 id로 지정된 파라미터에 해당 값을 주입한다.
+     * 3.몽고디비에서 'My Cart'를 찾아서 없으면 defaultIfEmpty 를 통해 새로 생성해 반환한다. 앞에서도
+     *   살펴본 전형적인 리액터 사용법이다.
+     * 4. 장바구니 데이터를 가져온 후에 가장 먼저 해야 할 일은 장바구니에 담겨 있는 상품이 있는지 확인하는 것이다.
+     * 전통적인 기존 방식이라면 for-each 반복문으로 처리했겠지만, 자바의 스트림 API 덕분에 CartItem 을 순회하면서
+     * 새로 장바구니에 담은 것과 동일한 종류의 상품이 이미 있는지 확인할 수 있다. findAny()는 Optional<CartItem> 을
+     * 반환하며, 같은 상품이 있다면 map() 내부에서 해당 상품의 수량만 증가시키고 장바구니를 Mono에 담아 반환한다.
+     * 5.새로 장바구니에 담은 상품이 장바구니에 담겨 있지 않은 상품이라면, 몽고디비에서 해당 상품을 조회 한후
+     * 6.업데이트된 장바구니를 몽고디비에 저장한다.
+     * 7.redirect:/ 반환하며 웹플럭스가 HTTP 요청을 / 위치로 리다이렉트한다.
+     */
+    // tag::3[]
+    @PostMapping("/add/{id}") // <1>
+    Mono<String> addToCart(@PathVariable String id) { // <2>
+        return this.cartRepository.findById("My Cart") //
+                .defaultIfEmpty(new Cart("My Cart")) // <3>
+                .flatMap(cart -> cart.getCartItems().stream() // <4>
+                        .filter(cartItem -> cartItem.getItem() //
+                                .getId().equals(id)) //
+                        .findAny() //
+                        .map(cartItem -> {
+                            cartItem.increment();
+                            return Mono.just(cart);
+                        }) //
+                        .orElseGet(() -> { // <5>
+                            return this.itemRepository.findById(id) //
+                                    .map(item -> new CartItem(item)) //
+                                    .map(cartItem -> {
+                                        cart.getCartItems().add(cartItem);
+                                        return cart;
+                                    });
+                        }))
+                .flatMap(cart -> this.cartRepository.save(cart)) // <6>
+                .thenReturn("redirect:/"); // <7>
+    }
+    // end::3[]
 
 }
