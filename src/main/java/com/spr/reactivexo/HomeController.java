fe1a608206823264d1f11a2d67b3322f99e1fe95
sungbo <ishimsungbo@gmail.com>
2022-11-03 14:17:20 +0900

    몽고디비를 이용 데이터 조회, 저장

--- src/main/java/com/spr/reactivexo/HomeController.java
+++ src/main/java/com/spr/reactivexo/HomeController.java
@@ -1,5 +1,6 @@
 package com.spr.reactivexo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,9 @@ public class HomeController {
     //주입하자
     private ItemRepository itemRepository;
     private CartRepository cartRepository;
+
+    @Autowired
+    private CartService cartService;
 
     public HomeController(ItemRepository itemRepository, CartRepository cartRepository) {
         this.itemRepository = itemRepository;
@@ -43,14 +47,18 @@ public class HomeController {
      * 3.몽고디비에서 'My Cart'를 찾아서 없으면 defaultIfEmpty 를 통해 새로 생성해 반환한다. 앞에서도
      *   살펴본 전형적인 리액터 사용법이다.
      * 4. 장바구니 데이터를 가져온 후에 가장 먼저 해야 할 일은 장바구니에 담겨 있는 상품이 있는지 확인하는 것이다.
-     * 전통적인 기존 방식이라면 for-each 반복문으로 처리했겠지만, 자바의 스트림 API 덕분에 CartItem 을 순회하면서
-     * 새로 장바구니에 담은 것과 동일한 종류의 상품이 이미 있는지 확인할 수 있다. findAny()는 Optional<CartItem> 을
-     * 반환하며, 같은 상품이 있다면 map() 내부에서 해당 상품의 수량만 증가시키고 장바구니를 Mono에 담아 반환한다.
-     * 5.새로 장바구니에 담은 상품이 장바구니에 담겨 있지 않은 상품이라면, 몽고디비에서 해당 상품을 조회 한후
+     *   전통적인 기존 방식이라면 for-each 반복문으로 처리했겠지만, 자바의 스트림 API 덕분에 CartItem 을 순회하면서
+     *   새로 장바구니에 담은 것과 동일한 종류의 상품이 이미 있는지 확인할 수 있다. findAny()는 Optional<CartItem> 을
+     *   반환하며, 같은 상품이 있다면 map() 내부에서 해당 상품의 수량만 증가시키고 장바구니를 Mono에 담아 반환한다.
+     * 5.새로 장바구니에 담은 상품이 장바구니에 담겨 있지 않은 상품이라면, 몽고디비에서 해당 상품을 조회 한 후 수량을
+     *   1로 지정하고 CartItem 에 담은 다음, CartItem 을 장바구니에 추가한 후에 장바구니를 반환한다. 이 과정은 장바구니에
+     *   동일한 상품이 없을 때만 수행되므로 하나의 람다식에서 모두 처리한다.
      * 6.업데이트된 장바구니를 몽고디비에 저장한다.
      * 7.redirect:/ 반환하며 웹플럭스가 HTTP 요청을 / 위치로 리다이렉트한다.
      */
+
     // tag::3[]
+    /*
     @PostMapping("/add/{id}") // <1>
     Mono<String> addToCart(@PathVariable String id) { // <2>
         return this.cartRepository.findById("My Cart") //
@@ -73,6 +81,14 @@ public class HomeController {
                         }))
                 .flatMap(cart -> this.cartRepository.save(cart)) // <6>
                 .thenReturn("redirect:/"); // <7>
+    }
+    */
+
+    //위의 소스를 서비스로 옮기면 간결해진다.
+    @PostMapping("/add/{id}") // <1>
+    Mono<String> addToCart(@PathVariable String id){
+        return this.cartService.addToCart("My Cart", id)
+                .thenReturn("redirect:/");
     }
     // end::3[]
 
